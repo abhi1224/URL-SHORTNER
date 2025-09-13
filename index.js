eb44a026e3b24c7f51443f4c59550313a0a8e587
Abhishek Verma <abhishek27501@gmail.com>
2025-09-13 14:02:33 +0530

    url-shortner UI(Server Side Rendering)

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require("express")
 const urlRoute = require("./routes/url")
 const { connectToMongoDB } = require('./connect')
 const URL = require('./models/url.model')
+const path = require('path')
+
+const staticRoute = require("./routes/staticRouter")
 
 const PORT = 8000
 
@@ -11,43 +14,39 @@ const app = express()
 connectToMongoDB("mongodb://127.0.0.1:27017/short-url")
 .then(() => console.log('MongoDB Connected !'))
 
+app.set("view engine", "ejs")
+app.set("views", path.resolve("./views"))
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use('/url', urlRoute);
+app.use('/', staticRoute);
 
 // Server side rendering 
 app.get('/test', async (req, res) => {
     const allUrls = await URL.find({});
     
-    return res.end(
-        `<html>
-        <head></head>
-        <body>
-            <ol>
-                ${allUrls.map((url) => `<li>${url.shortId} - ${url.redirectURL} - ${url.visitHistory.length}</li>`)}
-            </ol>
-        </body>
-        </html>`
-    )
+    return res.render("home",{
+        urls: allUrls,
+    })
 })
 
 app.get('/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
-    const entry = await URL
-        .findOneAndUpdate(
-        {
-            shortId
-        }, 
-        { $push:
-            {
-                visitHistory: {
-                    timestamp: Date.now()
-                }
-            } 
-        })
-    res.redirect(entry.redirectURL);
 
-})
+    const entry = await URL.findOneAndUpdate(
+        { shortId },
+        { $push: { visitHistory: { timestamp: Date.now() } } },
+        { new: true } // return the updated document
+    );
+
+    if (!entry) {
+        return res.status(404).json({ error: "Short URL not found" });
+    }
+
+    return res.redirect(entry.redirectURL);
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running at PORT http://localhost:${PORT}`);    
